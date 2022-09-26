27f016f5f8caf67b74aa9129b58def8422886b27
mcnxo2001 <minhvietnam147@gmail.com>
2022-09-26 19:31:10 +0700

    signin

---
+++ Login/login.js
@@ -0,0 +1,43 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATRkhlAvRakRxWvI8DQO4wG5rk76PG9Pc",
+    authDomain: "arduino-3b384.firebaseapp.com",
+    databaseURL: "https://arduino-3b384-default-rtdb.firebaseio.com",
+    projectId: "arduino-3b384",
+    storageBucket: "arduino-3b384.appspot.com",
+    messagingSenderId: "807572278992",
+    appId: "1:807572278992:web:f711223096d2c789a398d5",
+    measurementId: "G-57K4NWXNXN"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+function Login() {
+    Username = document.getElementById("Username").value;
+    Password = document.getElementById("Password").value;
+    if (Username == "" || Password == "") {
+        alert("Please enter your username and password");
+    }
+    else {
+        var starCountRef = firebase.database().ref('Account/' + Username);
+        starCountRef.on('value', (snapshot) => {
+            const data = snapshot.val();
+            try {
+                if (Password == data.Password) {
+                    window.location = ("https://mcnxo2001.github.io/PumpSystem/Main/main.html");
+                }
+                else {
+                    alert("Signup failed !!!");
+                }
+            }
+            catch(error){
+                alert("Signup failed !!!");
+            }
+        }
+        );
+    }
+}
+
+
